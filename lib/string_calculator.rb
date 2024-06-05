8de4c96d3fd286fe49bce1211fccd10c35c388c2
Sumit Sengar <sumit.sengar19996@gmail.com>
2024-06-05 22:00:55 +0530

    Added inital code to return 0 if string is empty

---
+++ lib/string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+  def add(input_string)
+    return 0 if input_string.empty?
+  end
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,15 @@
-# TDD without monkey patch for calculator_spec.rb
-require 'calculator.rb'
+require 'string_calculator.rb'
 
 RSpec.describe StringCalculator do
   describe '#add' do
     before do
       @string_calculator = StringCalculator.new
+    end
+
+    context 'when input string is empty' do
+      it 'should return 0' do
+        expect(@string_calculator.add("")).to eq(0)
+      end
     end
   end
 end

e93477198c0717237a0c4da6678b7b17b8624999
Sumit Sengar <sumit.sengar19996@gmail.com>
2024-06-05 22:57:48 +0530

    Code refactored and added some more test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,19 +2,34 @@ require 'byebug'
 class StringCalculator
   def add(input_string)
     return 0 if input_string.empty?
+    symbol = '+'
+    
+    numbers = get_numbers_from_string(input_string)
+    check_for_negatives(numbers)
 
+    #using inject by passing the symbol variable, which can be dynamic if required
+    numbers.inject(0, symbol.to_sym)
+  end
+
+  private
+
+  def get_numbers_from_string(input_string)
+    delimiter, numbers = get_delimeter_and_numbers(input_string)
+    numbers = numbers.split(delimiter).map(&:to_i)
+  end
+
+  def get_delimeter_and_numbers(input_string)
     delimiter = ","
+
     if input_string.start_with?("//")
       delimiter = input_string[2]
       input_string = input_string.split("\n", 2).last
     end
-    numbers = input_string.split(delimiter).map(&:to_i)
+    [delimiter, input_string]
+  end
 
+  def check_for_negatives(numbers)
     negatives = numbers.select { |num| num < 0 }
-    unless negatives.empty?
-      raise StandardError, "negative numbers not allowed: #{negatives.join(',')}"
-    end
-
-    numbers.sum
+    raise StandardError, "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
